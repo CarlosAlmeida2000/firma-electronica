1d935c938aba7c16444e1328bad170f451af1313
Carlosalmeida2000 <carlos.almeida2017@uteq.edu.ec>
2025-07-21 19:48:01 -0500

    first commit

---
+++ src/test/java/ec/gob/firmadigital/libreria/test/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.gob.firmadigital.libreria.test;
+
+/**
+ *
+ * @author jacob
+ */
+public class NewClass {
+    
+}
